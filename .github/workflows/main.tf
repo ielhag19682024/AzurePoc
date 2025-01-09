fe28715ee493bb499e68324995136508e1ec3a70
ielhag19682024 <ielhag@gmail.com>
2025-01-09 10:46:07 -0500

    Create main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,163 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "fa506ae2-e174-47a9-bafa-af7b0eefa394"
+}
+
+variable "resource_group" {
+  default = "azure-infra-architecture"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd1234!"
+}
+
+variable "vm_size" {
+  default = "Standard_B2s"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "azure-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "web-app-db-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "web" {
+  name                = "web-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "web_vm" {
+  name                = "web-server"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.web.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "web_vm" {
+  name                 = "web-extension"
+  virtual_machine_id   = azurerm_windows_virtual_machine.web_vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -Command \"Install-PackageProvider -Name NuGet -Force; Install-Module -Name 'NodeJs' -RequiredVersion '22.0'\""
+    }
+  SETTINGS
+}
+
+resource "azurerm_network_interface" "db" {
+  name                = "db-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "db_vm" {
+  name                = "db-server"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.db.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "9-RAW"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "db_extension" {
+  name                 = "db-extension"
+  virtual_machine_id   = azurerm_linux_virtual_machine.db_vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScript"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "bash -c 'sudo apt-get update; sudo apt-get install mssql-server -y'"
+    }
+  SETTINGS
+}
